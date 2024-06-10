372b066fa393f417836693d39c7ca98d17b0fd49
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-10 17:59:53 +0200

    Corretto refuso.

--- database/migrations/2024_06_10_155449_create_projects_table.php
+++ database/migrations/2024_06_10_155449_create_projects_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
             $table->string('name');
             $table->string('web_site'); 
